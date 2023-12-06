c593a7e7e56cb12bdcc05f41b5e6cc0973f3f67d
Dironmarek <77089372+Dironmarek@users.noreply.github.com>
2023-12-06 17:45:59 +0000

    added game files

--- HarryDogger.java
+++ HarryDogger.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Canvas;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyListener;
@@ -10,110 +11,100 @@ import static java.lang.Character.*;
 import java.awt.image.BufferedImage;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.net.URL;
 
-public class HarryDogger extends Canvas implements KeyListener, Runnable{
-    private BufferedImage back;
-    private boolean[] keys;
+import javax.imageio.ImageIO;
 
+public class HarryDogger extends Canvas implements KeyListener, Runnable {
 
-    public HarryDogger(){
-        setBackground(Color.black);
+  private BufferedImage back;
+  private boolean[] keys;
+  private Image background;
 
-
-        this.addKeyListener(this);
-        new Thread(this).start();
-    
-        setVisible(true);
-    }
-
-    public void update(Graphics window)
-  {
-    paint(window);
-  }
-
-  public void paint( Graphics window )
-  {
-    Graphics2D twoDGraph = (Graphics2D)window;
-
-    //take a snap shop of the current screen and same it as an image
-    //that is the exact same width and height as the current screen
-    if (back==null)
-      back = (BufferedImage)(createImage(getWidth(),getHeight()));
-
-      twoDGraph.drawImage(back, null, 0, 0);
-
-  }
-
-  public void keyPressed(KeyEvent e)
-  {
-    if (e.getKeyCode() == KeyEvent.VK_LEFT)
+  public HarryDogger() {
+    try
     {
+      URL url = getClass().getResource("ship.jpg");
+      background = ImageIO.read(url);
+    }
+    catch(Exception e)
+    {
+      //feel free to do something here
+    }
+    
+    this.addKeyListener(this);
+    new Thread(this).start();
+
+    setVisible(true);
+  }
+
+  public void update(Graphics window) {
+    paint(window);
+    window.drawImage(background, 600, 600, 600, 600, null);
+  }
+
+  public void paint(Graphics window) {
+    Graphics2D twoDGraph = (Graphics2D) window;
+
+    // take a snap shop of the current screen and same it as an image
+    // that is the exact same width and height as the current screen
+    if (back == null)
+      back = (BufferedImage) (createImage(getWidth(), getHeight()));
+
+    twoDGraph.drawImage(back, null, 0, 0);
+
+  }
+
+  public void keyPressed(KeyEvent e) {
+    if (e.getKeyCode() == KeyEvent.VK_LEFT) {
       keys[0] = true;
     }
-    if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-    {
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
       keys[1] = true;
     }
-    if (e.getKeyCode() == KeyEvent.VK_UP)
-    {
+    if (e.getKeyCode() == KeyEvent.VK_UP) {
       keys[2] = true;
     }
-    if (e.getKeyCode() == KeyEvent.VK_DOWN)
-    {
+    if (e.getKeyCode() == KeyEvent.VK_DOWN) {
       keys[3] = true;
     }
-    if (e.getKeyCode() == KeyEvent.VK_SPACE)
-    {
+    if (e.getKeyCode() == KeyEvent.VK_SPACE) {
       keys[4] = true;
     }
     repaint();
   }
 
-  public void keyReleased(KeyEvent e)
-  {
-    if (e.getKeyCode() == KeyEvent.VK_LEFT)
-    {
+  public void keyReleased(KeyEvent e) {
+    if (e.getKeyCode() == KeyEvent.VK_LEFT) {
       keys[0] = false;
     }
-    if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-    {
+    if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
       keys[1] = false;
     }
-    if (e.getKeyCode() == KeyEvent.VK_UP)
-    {
+    if (e.getKeyCode() == KeyEvent.VK_UP) {
       keys[2] = false;
     }
-    if (e.getKeyCode() == KeyEvent.VK_DOWN)
-    {
+    if (e.getKeyCode() == KeyEvent.VK_DOWN) {
       keys[3] = false;
     }
-    if (e.getKeyCode() == KeyEvent.VK_SPACE)
-    {
+    if (e.getKeyCode() == KeyEvent.VK_SPACE) {
       keys[4] = false;
     }
     repaint();
   }
 
-  public void keyTyped(KeyEvent e)
-  {
-    //no code needed here
+  public void keyTyped(KeyEvent e) {
+    // no code needed here
   }
 
-  public void run()
-  {
-    try
-    {
-      while(true)
-      {
+  public void run() {
+    try {
+      while (true) {
         Thread.currentThread().sleep(5);
         repaint();
       }
-    }catch(Exception e)
-    {
+    } catch (Exception e) {
     }
   }
 
-  
-
-    
 }
